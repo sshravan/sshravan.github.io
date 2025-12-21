6820981109f1197d6569a32c8af6f81230534127
Shravan Srinivasan <ShravanSrinivasan1@gmail.com>
2025-12-21 02:03:12 -0500

    updated website

---
+++ assets/js/search-data.js
@@ -0,0 +1,12 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },];
